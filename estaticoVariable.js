2e429d57a1d41456c24539048ab745c7e758a746
JuanDRosero <43081291+JuanDRosero@users.noreply.github.com>
2022-06-07 10:53:35 -0500

    Ya cambia el algoritmo según se selccione

--- estaticoVariable.js
+++ estaticoVariable.js
@@ -98,6 +98,7 @@ function IniciarMejor(){
 
 //que ordena la ram en particiones de objetos peor Ajuste (de pequeño a grande) (1,4096,4096,2048,1024,1024.1024,512,512,512,256,256)
 function IniciarPeor(){
+    console.log("Cambio esta cosa");
     procesos=
     [
         {
@@ -235,7 +236,33 @@ function pintado(){
         inicio+=(procesos[i].Tamaño)*(700/(16*1048576))   
     }
 }
-IniciarMejor();
+let selectElement;
+window.onload = function(){
+    IniciarMejor();
+    selectElement= document.getElementById("tbl2");
+    selectElement.addEventListener('change', (event) => {
+        switch(selectElement.value){  
+            case "1":     //Primer Ajuste
+                IniciarMejor();
+                break;
+            case "2":
+                IniciarMejor();
+                break;
+            case "3":
+                IniciarPeor();
+                break;
+            default:
+                console.log(selectElement.value);
+        }
+        pintado();
+    });
+    pintado();
+    }
+    
+    
+
+
+
 
 /*
 IniciarMejor();
